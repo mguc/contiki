c8cf63e1bc5a7ab222ae5a1f844ca629262433a5
Marko Gucanin <marko.gucanin@soom-it.ch>
2016-02-02 19:10:45 +0100

    removed build folder from gitignore added build scripts for every platform separately

---
+++ build/jenkins/build-cp6-contiki-slip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+HERE=$(dirname $0)
+
+echo ----- DOCKER build TR2 $(pwd)
+pushd build/docker/builder/
+docker build -t tr2contiki .
+popd
+
+echo ----- DOCKER run TR2 $(pwd)
+rm -fr ./dist/jn516x-cp6.bin && mkdir -p dist
+echo mount $(pwd) as /source
+
+# build cp6 JN5168 binary
+docker run --rm -a stdin -a stdout -a stderr -v $(pwd)/dist:/out -v $(pwd):/source tr2contiki bash -c 'cd /source/neeo/slip-radio && PATH=$PATH:/builder/bin WITH_FEM=1 TARGET=jn516x-neeo make distclean all && cp ./slip-radio.jn516x-neeo.bin /out/jn516x-cp6.bin'
