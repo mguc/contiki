41bb057497c356334a7aac3f7f25f317f11bd7fa
Fredrik Österlind <fredrik@thingsquare.com>
2016-02-19 09:35:43 +0100

    removed comm-tests-specific configuration, let's instead use the neeo-conf.h only

--- neeo/comm-tests/project-conf.h
+++ neeo/comm-tests/project-conf.h
@@ -1,11 +1,6 @@
 #ifndef _PROJECT_CONF_H
 #define _PROJECT_CONF_H
 
-#define MKEYSEC_CONF_DEFAULT_KEY 1
-#define RF_CHANNEL 25
-#define CC2538_RF_CHANNEL 25
-#define IEEE802154_CONF_PANID           0xABCD
-
 #include "../neeo-conf.h"
 
 #endif /* _PROJECT_CONF_H */
