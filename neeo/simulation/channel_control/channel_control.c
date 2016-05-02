3a64be409059df65028ec5011ab939d76c7f185d
Marko Gucanin <marko.gucanin@soom-it.ch>
2016-05-02 11:57:47 +0200

    fixed some syntax errors

--- neeo/simulation/channel_control/channel_control.c
+++ neeo/simulation/channel_control/channel_control.c
@@ -1,5 +1,6 @@
 #include "channel_control.h"
 #include "contiki.h"
+#include "radio.h"
 
 #define DISCOVERY_DUTY_CYCLE CLOCK_SECOND/2
 static struct etimer discovery_duty_cycle;
@@ -7,14 +8,16 @@ static struct etimer discovery_duty_cycle;
 #define DEBUG DEBUG_FULL
 #include "net/ip/uip-debug.h"
 
-static uint8_t
+AUTOSTART_PROCESSES(&channel_control);
+
+static int
 get_rf_channel(void)
 {
   radio_value_t chan;
 
   NETSTACK_RADIO.get_value(RADIO_PARAM_CHANNEL, &chan);
   PRINTF("Current channel: %u", chan);
-  return (uint8_t)chan;
+  return (int)chan;
 }
 
 static int
@@ -24,7 +27,7 @@ set_rf_channel(radio_value_t chan)
     PRINTF("Not a valid channel: %u", chan);
     return -1;
   }
-  PRINTF("Setting new channel: %u", chan)
+  PRINTF("Setting new channel: %u", chan);
   NETSTACK_RADIO.set_value(RADIO_PARAM_CHANNEL, chan);
   return 0;
 }
@@ -41,17 +44,17 @@ static void discover_callback(struct simple_udp_connection *c,
     etimer_stop(&discovery_duty_cycle);  
     uint32_t buf_len = 48;
     char buf[48] = {0};
-    print_addr(source_addr, buf, &buf_len);
+    // print_addr(source_addr, buf, &buf_len);
     PRINTF("Dicovery from: %s ", buf);
     PRINTF("I'm NBR!\n");
     buf_len = 48;
     memcpy(buf, "Y ", 2);
     buf_len -= 2;
-    print_addr(&tun_address, buf+2, &buf_len);
+    // print_addr(&tun_address, buf+2, &buf_len);
     c->remote_port = source_port;
     simple_udp_sendto(c, buf, buf_len+2, source_addr);
   }
-  if(strcmp(char*)data, "H") == 0{
+  if(strcmp((char*)data, "H") == 0) {
     PRINTF("Received heartbeat\n");
     char response = 'Y';
     c->remote_port = source_port;
@@ -65,29 +68,33 @@ PROCESS_THREAD(channel_control, ev, data)
   static struct simple_udp_connection server_conn;
   
   static int current_discovery_channel_index = 0;
+  static int *p_message;
   
   PROCESS_BEGIN();
   
   simple_udp_register(&server_conn, 3200, NULL, 0, discover_callback);
 
   PRINTF("Channel control process started\n");
+  PRINTF("Current channel: %i", get_rf_channel());
 
   while(1) {
     
     PROCESS_YIELD();
-    
+    p_message = (int*) data;
     switch(ev){
       case PROCESS_EVENT_MSG:
-        switch(data){
+        switch(*p_message){
           case DISCOVER:
-            int current_discovery_channel_index = 0;
-            set_rf_channel(discovery_channels[current_discovery_channel_index++])
+            current_discovery_channel_index = 0;
+            set_rf_channel(discovery_channels[current_discovery_channel_index++]);
             etimer_set(&discovery_duty_cycle, DISCOVERY_DUTY_CYCLE);
+            break;
+          default:
             break;
         }
         break;
       case PROCESS_EVENT_TIMER:
-        if(current_discovery_channel_index >= DISCOVERY_CHANNELS_COUNT)
+        if(current_discovery_channel_index >= sizeof(discovery_channels)/sizeof(radio_value_t))
           current_discovery_channel_index = 0;
         set_rf_channel(discovery_channels[current_discovery_channel_index++]);
         etimer_set(&discovery_duty_cycle, DISCOVERY_DUTY_CYCLE);  
--- neeo/simulation/channel_control/channel_control.h
+++ neeo/simulation/channel_control/channel_control.h
@@ -1,34 +1,39 @@
 #ifndef CHANNEL_CONTROL_H
 #define CHANNEL_CONTROL_H
 
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+
 #define FIRST_CHANNEL 11
 #define LAST_CHANNEL 26
 
-typedef enum channel_event_e = {
+typedef enum channel_event_e {
   DISCOVER,
   BEST_CHANNEL
 } channel_event_t;
 
-typedef enum channel_status_e = {
+typedef enum channel_status_e {
   BAD,
   OK,
   VERY_GOOD
 } channel_status_t;
 
-typedef struct channel_quality_s = {
+typedef struct channel_quality_s {
   channel_status_t status;
 } channel_quality_t;
 
-typedef struct channel_s = {
+typedef struct channel_s {
   uint8_t number;
   channel_quality_t quality;
 } channel_t;
 
 const radio_value_t discovery_channels[] = {11, 16, 21, 26};
-#define DISCOVERY_CHANNELS_COUNT do { \
+#define DISCOVERY_CHANNELS_COUNT \
+do { \
   sizeof(discovery_channels)/sizeof(radio_value_t); \
-} while(0);
+} while(0)
 
-PROCESS(channel_control_process, "Channel control process");
+PROCESS(channel_control, "Channel control process");
 
 #endif /* CHANNEL_CONTROL_H */
