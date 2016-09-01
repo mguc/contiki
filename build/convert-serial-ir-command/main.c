86c91880aeca7c32449830699544530d8d32f09b
michuNEEO <michael@neeo.com>
2016-09-01 16:48:01 +0200

    add expermental ir code to uart payload converter

---
+++ build/convert-serial-ir-command/main.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define SWAP_BYTES16(__x) ((((__x) & 0xff) << 8) | ((__x) >> 8))
+#define IRBLASTER_START 0x0000
+
+void sendir_handler(uint32_t freq_val, uint16_t offset_val, char sequence[12288]) {
+  uint16_t count_val = 1;
+  uint16_t send_buf[10*1024];
+  char* p = NULL;
+  uint16_t buf;
+
+  //i=0&f=36000&c=10&o=3&s=21.40.21.40.10.50.10.50'
+
+  strcpy((char*)send_buf, "!I");
+  send_buf[1] = SWAP_BYTES16(IRBLASTER_START);
+  send_buf[2] = SWAP_BYTES16(freq_val & 0x0000ffff);
+  send_buf[3] = SWAP_BYTES16(freq_val >> 16);
+  send_buf[4] = SWAP_BYTES16(count_val);
+  send_buf[5] = SWAP_BYTES16(offset_val);
+
+//0x21, 0x49, 0x00, 0x00, 0x75, 0x30, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x15, 0x00, 0x28, 0x00, 0x15, 0x00, 0x28, 0x00, 0x0A, 0x00, 0x32, 0x00, 0x0A, 0x00, 0x32, ~
+
+  int i = 6;
+  p = sequence;
+  do {
+    buf = strtoul(p, &p, 10);
+    printf("%u -- ", buf & 0xff);
+    printf("%u\n", (buf >> 8) & 0xff);
+    send_buf[i++] = SWAP_BYTES16(buf);
+    if(*p == 0)
+      break;
+    p++;
+  } while(*p);
+
+  if(i < 7) {
+    printf("sequnce too short!\n");
+    return;
+  }
+
+  if(offset_val > (i - 6)) {
+    printf("request error: offset is greater than payload lenght (%u > %u)! Response: 400!\n", offset_val, i - 6);
+    return;
+  }
+
+  uint8_t *pnt = &(send_buf[0]);
+  for (int n = 0; n < i*2; n++) {
+    printf("0x%.2X, ", pnt[n]);
+  }
+}
+
+int main(int argc, const char * argv[]) {
+    printf("IR to UART MARSHALLER!\n");
+
+    //sendir,1:1,1,37825,1,1,171,171,21,64,21,64,21,64,21,21,21,21,21,21,21,21,21,21,21,64,21,64,21,64,21,21,21,21,21,21,21,21,21,21,21,64,21,64,21,64,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,21,64,21,64,21,64,21,64,21,64,21,1776
+    sendir_handler(37825, 0, "171.171.21.64.21.64.21.64.21.21.21.21.21.21.21.21.21.21.21.64.21.64.21.64.21.21.21.21.21.21.21.21.21.21.21.64.21.64.21.64.21.21.21.21.21.21.21.21.21.21.21.21.21.21.21.21.21.64.21.64.21.64.21.64.21.64.21.1776");
+    return 0;
+}
